718d217a071da5f5b866a5a5b44c858ba152f6ba
Vinicius Albuquerque (Apolo) <62688708+apoloapolo@users.noreply.github.com>
2022-03-06 03:12:07 -0300

    commit

---
+++ Vinicius_Albuquerque_(Apolo)_LISTA_01/src/estoque_questao01/Estoque.java
@@ -0,0 +1,41 @@
+package estoque_questao01;
+
+public class Estoque {
+	private Produto produto;
+	private int qtdAtual;
+	private int qtdMinima;
+
+	public Estoque() {
+		this.produto = null;
+		this.qtdAtual = 0;
+		this.qtdMinima = 0;
+	}
+
+	public Estoque(Produto produto, int qtdAtual, int qtdMinima) {
+		this.produto = produto;
+		this.qtdAtual = qtdAtual;
+		this.qtdMinima = qtdMinima;
+	}
+
+	public void mudarQtdMinima(int qtdMinima) {
+		this.qtdMinima = qtdMinima;
+	}
+
+	public void reporEstoque(int qtd) {
+		this.qtdAtual += qtd;
+	}
+
+	public void darBaixa(int qtd) {
+		if (this.qtdAtual - qtd >= this.qtdMinima) {
+			this.qtdAtual -= qtd;
+		} else {
+			System.out.println(
+					"Baixa não realizada: quantidade que seria retirada deixa o estoque abaixo da quantidade mínima");
+		}
+	}
+	
+	public void relatorio() {
+		System.out.printf("");
+	}
+
+}
---
+++ Vinicius_Albuquerque_(Apolo)_LISTA_01/src/estoque_questao01/Produto.java
@@ -0,0 +1,21 @@
+package estoque_questao01;
+
+public class Produto {
+	private String codigo;
+	private String nome;
+
+	public Produto() {
+		this.codigo = null;
+		this.nome = null;
+	}
+
+	public Produto(String codigo, String nome) {
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+
+	public void mudarNome(String novoNome) {
+		this.nome = novoNome;
+	}
+
+}
